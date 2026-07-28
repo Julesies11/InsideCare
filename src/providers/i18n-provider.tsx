5e12821daedf2638f16f604050a60d2574d1a4f5
Julian <julian_@hotmail.com>
2026-07-28 13:39:53 +0800

    fix: vercel build errors

--- src/providers/i18n-provider.tsx
+++ src/providers/i18n-provider.tsx
@@ -14,13 +14,13 @@ import { I18nProviderProps, type Language } from '@/i18n/types';
 import { DirectionProvider as RadixDirectionProvider } from '@radix-ui/react-direction';
 import { IntlProvider } from 'react-intl';
 import { getData, setData } from '@/lib/storage';
-import '@formatjs/intl-relativetimeformat/polyfill';
-import '@formatjs/intl-relativetimeformat/locale-data/en';
-import '@formatjs/intl-relativetimeformat/locale-data/de';
-import '@formatjs/intl-relativetimeformat/locale-data/es';
-import '@formatjs/intl-relativetimeformat/locale-data/fr';
-import '@formatjs/intl-relativetimeformat/locale-data/ja';
-import '@formatjs/intl-relativetimeformat/locale-data/zh';
+import '@formatjs/intl-relativetimeformat/polyfill.js';
+import '@formatjs/intl-relativetimeformat/locale-data/en.js';
+import '@formatjs/intl-relativetimeformat/locale-data/de.js';
+import '@formatjs/intl-relativetimeformat/locale-data/es.js';
+import '@formatjs/intl-relativetimeformat/locale-data/fr.js';
+import '@formatjs/intl-relativetimeformat/locale-data/ja.js';
+import '@formatjs/intl-relativetimeformat/locale-data/zh.js';
 
 const getInitialLanguage = () => {
   const urlParams = new URLSearchParams(window.location.search);
